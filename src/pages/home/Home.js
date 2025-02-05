3a8763e9adc23c00141c22bf755ba823f333c5c1
Kyma-ug <markyokuhaire18@gmail.com>
2025-02-05 18:00:38 +0300

    Changed the image names on the partners carousel

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -23,17 +23,17 @@ import ResourceCenter from '../../components/resourcecenter/ResourceCenter';
 
 
 const partners = [
-  { id: 1, name: 'Partner 1', logo: addlogo, website: 'https://add.org.uk/madipha/' },
-  { id: 2, name: 'Partner 2', logo: stoptbpart, website: 'https://www.stoptb.org/' },
-  { id: 3, name: 'Partner 3', logo: amplify, website: 'https://amplifychange.org/' },
-  { id: 4, name: 'Partner 6', logo: drf, website: 'https://www.disabilityrightsfund.org/' },
+  { id: 1, name: 'ADD International', logo: addlogo, website: 'https://add.org.uk/madipha/' },
+  { id: 2, name: 'The Stop TB Partnership', logo: stoptbpart, website: 'https://www.stoptb.org/' },
+  { id: 3, name: 'Amplify Change', logo: amplify, website: 'https://amplifychange.org/' },
+  { id: 4, name: 'The Disability Rights Fund', logo: drf, website: 'https://www.disabilityrightsfund.org/' },
   // { id: 5, name: 'Partner 10', logo: stoptb, website: 'https://ustp.org.ug/' },
-  { id: 6, name: 'Partner 5', logo: frontline, website: 'https://frontlineaids.org/' },
-  { id: 7, name: 'Partner 8', logo: tag, website: 'https://www.treatmentactiongroup.org/' },
-  { id: 8, name: 'Partner 4', logo: dfhug, website: 'https://x.com/DhfUganda' },
-  { id: 9, name: 'Partner 7', logo: nerve, website: 'https://www.wegotnerve.com/' },
-  { id: 10, name: 'Partner 10', logo: viiv, website: 'https://viivhealthcare.com/' },
-  { id: 11, name: 'Partner 9', logo: voice, website: 'https://voiceuganda.com/' },
+  { id: 6, name: 'Frontline AIDS', logo: frontline, website: 'https://frontlineaids.org/' },
+  { id: 7, name: 'Treatment Action Group', logo: tag, website: 'https://www.treatmentactiongroup.org/' },
+  { id: 8, name: 'DHF Uganda', logo: dfhug, website: 'https://x.com/DhfUganda' },
+  { id: 9, name: 'We Got Nerve', logo: nerve, website: 'https://www.wegotnerve.com/' },
+  { id: 10, name: 'VIIV Health Care', logo: viiv, website: 'https://viivhealthcare.com/' },
+  { id: 11, name: 'Voice Uganda', logo: voice, website: 'https://voiceuganda.com/' },
   
 ];
 
